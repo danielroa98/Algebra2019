d12dab2373137a23552005c193db3d593ce523c3
Daniel Roa <daniel.roa98@gmail.com>
2019-07-16 12:32:32 +0200

    Exceptions and implementations

--- Java Workspace/Java7/src/org/danielRoa/CustomExceptions.java
+++ Java Workspace/Java7/src/org/danielRoa/CustomExceptions.java
@@ -7,19 +7,26 @@ public class CustomExceptions {
 		double a = 1;
 		double b = 0;
 		
-		double res = divide(a, b);
+		double res = -1;
+		
+		try {
+			res = divide (a,b);
+		} catch (DivideWithZeroException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		System.out.println(res);
 		
 	}//fin MAIN
 
-	private static double divide(double a, double b) {
-		// TODO Auto-generated method stub
+	private static double divide(double a, double b) throws DivideWithZeroException {
+
 		if (b == 0) {
 			throw new DivideWithZeroException();
 		}
 		
 		return a / b;
-	}
+	}//fin DIVIDE
 	
 }//fin CLASS
--- Java Workspace/Java7/src/org/danielRoa/DivideWithZeroException.java
+++ Java Workspace/Java7/src/org/danielRoa/DivideWithZeroException.java
@@ -2,4 +2,8 @@ package org.danielRoa;
 
 public class DivideWithZeroException extends Exception {
 
+	public DivideWithZeroException() {
+		super("You cannot divide by zero");
+	}
+	
 }
