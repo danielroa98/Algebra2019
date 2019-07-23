63cb83a68d75d2629227ce93ab070e75ade43c32
Daniel Roa <daniel.roa98@gmail.com>
2019-07-23 12:52:49 +0200

    Beginning final project

--- AndroidStudioProjects/FinalProject/app/src/main/java/org/danielroa/finalproject/DBHelper.java
+++ AndroidStudioProjects/FinalProject/app/src/main/java/org/danielroa/finalproject/DBHelper.java
@@ -1,4 +1,55 @@
 package org.danielroa.finalproject;
 
-public class DBHelper {
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.HashMap;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String dbName = "MyDBName.db";
+    public static final String contactsTableName = "Contacts";
+    public static final String contactsId = "id";
+    public static final String contactsName = "Name";
+    public static final String contactsEmail = "Email";
+    public static final String contactsStreet = "Street";
+    public static final String contactsCity = "Place";
+    public static final String contactsPhone = "Phone";
+    private HashMap hp:
+
+    /*public DBHelper(Context context){
+        super(context, dbName, null, 1);
+    }*/
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(
+                "Create table Contacts" +
+                        "(id integer primary key, Name text, Phone text, Email text, Street text, Place text)"
+        );
+    }
+
+    public boolean insertContact(String name, String phone, String email, String street, String place){
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        ContentValues cV = new ContentValues();
+
+        cV.put("Name", name);
+        cV.put("Phone", phone);
+        cV.put("Name", email);
+        cV.put("Name", street);
+        cV.put("Name", place);
+
+        db.update("Contacts", cV, "id = ?", new String[] {Integer.toString(id)});
+
+        return true;
+    }
+
+    public Integer deleteContact(Integer id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete("Contacts", "id = ?", new String[] {Integer.toString(id)});
+    }
+
 }
--- AndroidStudioProjects/FinalProject/app/src/main/java/org/danielroa/finalproject/MainActivity.java
+++ AndroidStudioProjects/FinalProject/app/src/main/java/org/danielroa/finalproject/MainActivity.java
@@ -2,8 +2,13 @@ package org.danielroa.finalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String extraMessage = "Message";
+    private ListView obj;
+    DBHelper mydb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
